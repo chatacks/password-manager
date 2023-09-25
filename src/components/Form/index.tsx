c31f6a134331e22e42c5aa21ddadcb4d553329be
Thyago <chatack.tc@gmail.com>
2023-09-25 18:10:20 -0300

    [FEAT] Requisito 7

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,14 +6,25 @@ import { RegistryType } from '../../type';
 function Patern() {
   const [display, setDisplay] = useState(false);
   const [receiveInfos, setReceiveInfos] = useState<RegistryType[]>([]);
+  const [isCheck, setIsCheck] = useState(false);
 
   // função para controlar (display: 'none') do Form
   const handleDisplayClick = () => {
     setDisplay(!display);
   };
 
+  const handleCheckBoxClick = () => {
+    setIsCheck((terms) => !terms);
+  };
+
   const setReceive = (newRegistry: RegistryType) => {
     setReceiveInfos([...receiveInfos, newRegistry]);
+  };
+
+  const removeClick = (key: string) => {
+    setReceiveInfos(() => {
+      return receiveInfos.filter((infos) => infos.service !== key);
+    });
   };
 
   return (
@@ -25,20 +36,40 @@ function Patern() {
       {display
         ? null
         : <button onClick={ handleDisplayClick }> Cadastrar nova senha</button> }
-      <div>
-        <ul>
-          {receiveInfos.length ? (receiveInfos.map((info) => (
-            <li key={ info.service }>
-              <a href={ info.url }>
-                {info.service}
-              </a>
-              <p>{info.login}</p>
-              <p>{info.password}</p>
-            </li>
-          )))
-            : ('Nenhuma senha cadastrada')}
-        </ul>
-      </div>
+      <section>
+        <div>
+          <label htmlFor="hide-password">Esconder senhas</label>
+          <input
+            onChange={ handleCheckBoxClick }
+            checked={ isCheck }
+            type="checkbox"
+            name="hide-password"
+            id="hide-password"
+          />
+        </div>
+        <div>
+          <ul>
+            {receiveInfos.length ? (receiveInfos.map((info) => (
+              <li key={ info.service }>
+                <a href={ info.url }>
+                  {info.service}
+                </a>
+                <p>{info.login}</p>
+                { isCheck ? '******' : <p>{info.password}</p>}
+                <div>
+                  <button
+                    data-testid="remove-btn"
+                    onClick={ () => removeClick(info.service) }
+                  >
+                    Remove
+                  </button>
+                </div>
+              </li>
+            )))
+              : ('Nenhuma senha cadastrada')}
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
